fb4aed46e3c15854830a4fd48cdb5e2a21a2858a
Sdjurdjevic <hppc@hp>
2017-12-07 14:23:35 +0100

    Changement Controleur Representation

--- FestivalBilleterie-GR5/src/controleur/CtrlLesRepresentations.java
+++ FestivalBilleterie-GR5/src/controleur/CtrlLesRepresentations.java
@@ -2,11 +2,15 @@ package controleur;
 
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import modele.dao.RepresentationDao;
+import modele.metier.Representation;
 import vue.VueLesRepresentations;
 
 public class CtrlLesRepresentations extends CtrlGenerique implements WindowListener{
     
-    private final DaoRepresentation daoRepresentation = new DaoRepresentation();
+    private final RepresentationDao daoRepresentation = new RepresentationDao();
     private ArrayList<Representation> lesRepresentations;
    
     public CtrlLesRepresentations(CtrlPrincipal ctrlPrincipal) {
@@ -15,6 +19,32 @@ public class CtrlLesRepresentations extends CtrlGenerique implements WindowListe
         lesRepresentationsAfficher();
         vue.addWindowListener(this);
     }
+    
+    public void  representationQuitter(){
+        this.getCtrlPrincipal().action(EnumAction.MENU_REPRESENTATION_QUITTER);
+    }
+    
+    
+    public void representationAfficher() {
+        String msg = ""; // message à afficher en cas d'erreur
+        ((VueRepresentation) vue).getModeleTableEquipier().setRowCount(0);
+        String[] titresColonnes = {"Nom", "Prenom", "Volontaire"};
+        ((VueEquipiers) vue).getModeleTableEquipier().setColumnIdentifiers(titresColonnes);
+        try {
+            String[] ligneDonnees = new String[3];
+            lesEquipiers = daoEquipier.getAll();
+            for (Equipier unEquipier : lesEquipiers) {
+                ligneDonnees[0] = unEquipier.getNom();
+                ligneDonnees[1] = unEquipier.getPrenom();
+                ligneDonnees[2] = (unEquipier.isDimanche() ? "OUI" : "");
+                ((VueEquipiers) vue).getModeleTableEquipier().addRow(ligneDonnees);
+            }
+        } catch (Exception ex) {
+            msg = "CtrlEquipiers - equipiersAfficher() - " + ex.getMessage();
+            JOptionPane.showMessageDialog(vue, msg, "Affichage des équipiers", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
     
     
 
